a8c11ecf69615747299fcfaf49971fef8b262da0
blue_islands <h.aoshima@livlog.jp>
2021-06-02 23:34:11 +0900

    動作確認

---
+++ src/main/java/jp/livlog/austin/FirstStepsApplication.java
@@ -0,0 +1,44 @@
+package jp.livlog.austin;
+
+import org.restlet.Application;
+import org.restlet.Component;
+import org.restlet.Restlet;
+import org.restlet.data.Protocol;
+import org.restlet.routing.Router;
+
+import jp.livlog.austin.resource.HelloWorldResource;
+
+public class FirstStepsApplication extends Application {
+
+    /**
+     * Creates a root Restlet that will receive all incoming calls.
+     */
+    @Override
+    public synchronized Restlet createInboundRoot() {
+
+        // Create a router Restlet that routes each call to a new instance of HelloWorldResource.
+        final var router = new Router(this.getContext());
+
+        // Defines only one route
+        router.attach("/hello", HelloWorldResource.class);
+
+        return router;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+        // Create a new Component.
+        final var component = new Component();
+
+        // Add a new HTTP server listening on port 8182.
+        component.getServers().add(Protocol.HTTP, 8182);
+
+        // Attach the sample application.
+        component.getDefaultHost().attach("/firstSteps",
+                new FirstStepsApplication());
+
+        // Start the component.
+        component.start();
+    }
+}
---
+++ src/main/java/jp/livlog/austin/resource/HelloWorldResource.java
@@ -0,0 +1,17 @@
+package jp.livlog.austin.resource;
+
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+/**
+ * Resource which has only one representation.
+ */
+public class HelloWorldResource extends ServerResource {
+
+    @Get
+    public String represent() {
+
+        return "hello, world";
+    }
+
+}
